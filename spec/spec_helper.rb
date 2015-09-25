9b988578c80c03afdc6897d17e19640c79879332
poojamokariya <pooja.tps@gmail.com>
2015-09-25 19:10:53 +0530

    Adds sessions controller create action

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,9 +39,12 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  
   #Including to test requests
   config.include Request::JsonHelpers, :type => :controller
   config.include Request::HeadersHelpers, :type => :controller
+  config.include Devise::TestHelpers, :type => :controller
+
 
   config.before(:each, type: :controller) do
     include_default_accept_headers
