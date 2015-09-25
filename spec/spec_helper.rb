aec9b8c1943182afb6eb9d71d7a4fe6d29fd8026
poojamokariya <pooja.tps@gmail.com>
2015-09-25 19:00:47 +0530

    Refactors test headers for each request

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,5 +41,10 @@ RSpec.configure do |config|
   config.order = "random"
   #Including to test requests
   config.include Request::JsonHelpers, :type => :controller
+  config.include Request::HeadersHelpers, :type => :controller
+
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
 
 end
